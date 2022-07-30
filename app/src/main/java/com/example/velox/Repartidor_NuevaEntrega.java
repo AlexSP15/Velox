070e08eedbacc5d9fa4dd9b49bd171dbaeceac2b
JuanDiego20 <85000489+JuanDiego20@users.noreply.github.com>
2022-07-29 23:32:08 -0500

    Commit Nueva_Entrega

---
+++ app/src/main/java/com/example/velox/Repartidor_NuevaEntrega.java
@@ -0,0 +1,14 @@
+package com.example.velox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Repartidor_NuevaEntrega extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_repartidor_nueva_entrega);
+    }
+}
