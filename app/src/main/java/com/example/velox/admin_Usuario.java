f544f6f1a27c981e89802e1b1d202458864980fe
Trane19 <r193609714@utna.edu.mx>
2022-08-03 00:21:54 -0500

    Diseño pestañas admin

---
+++ app/src/main/java/com/example/velox/admin_Usuario.java
@@ -0,0 +1,14 @@
+package com.example.velox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class admin_Usuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_usuario);
+    }
+}
