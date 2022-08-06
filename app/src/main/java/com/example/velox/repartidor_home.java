ae420c072a6a75cc9fc6941d45ef06a5a06f0a7b
JuanDiego20 <85000489+JuanDiego20@users.noreply.github.com>
2022-08-05 23:27:10 -0500

    commit_repartidorHome

--- app/src/main/java/com/example/velox/repartidor_home.java
+++ app/src/main/java/com/example/velox/repartidor_home.java
@@ -2,13 +2,33 @@ package com.example.velox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class repartidor_home extends AppCompatActivity {
+    private ImageView btnRepEntrega;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_repartidor_home);
+        bindUI();
+        btnRepEntrega.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view ){
+                gotoMain();
+            }
+
+        });
+    }
+    private void bindUI(){
+        btnRepEntrega = (ImageView) findViewById(R.id.btnRepEntrega);
+    }
+    private void gotoMain(){
+        Intent intent = new Intent(repartidor_home.this,Repartidor_Rastreo.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 }
