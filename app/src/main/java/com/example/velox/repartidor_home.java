9ee5658319cc5601435a2cc2c9449d73bb62327d
JuanDiego20 <85000489+JuanDiego20@users.noreply.github.com>
2022-08-09 18:56:47 -0500

    Menu Repartidor

--- app/src/main/java/com/example/velox/repartidor_home.java
+++ app/src/main/java/com/example/velox/repartidor_home.java
@@ -2,13 +2,43 @@ package com.example.velox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class repartidor_home extends AppCompatActivity {
+    private ImageView btnEntregaR, btnNuevaR;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_repartidor_home);
+        bindUI();
+        btnEntregaR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoMain();
+            }
+        });
+        btnNuevaR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               gotoEntrega();
+            }
+        });
+    }
+    private void bindUI(){
+        btnEntregaR = (ImageView) findViewById(R.id.btnRepEntrega);
+        btnNuevaR = (ImageView) findViewById(R.id.btnNewEnvioRep);
+    }
+    private void gotoMain(){
+        Intent intent = new Intent(repartidor_home.this,Repartidor_Rastreo.class);
+        startActivity(intent);
+    }
+
+    private void gotoEntrega(){
+        Intent intent = new Intent(repartidor_home.this,Repartidor_NuevaEntrega.class);
+        startActivity(intent);
     }
 }
