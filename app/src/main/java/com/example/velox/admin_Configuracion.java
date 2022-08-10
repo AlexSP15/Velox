97ffa8a08200b694686bb3121622d58d342493e5
Trane19 <r193609714@utna.edu.mx>
2022-08-09 19:07:41 -0500

    Admin parte 3

--- app/src/main/java/com/example/velox/admin_Configuracion.java
+++ app/src/main/java/com/example/velox/admin_Configuracion.java
@@ -1,14 +1,33 @@
 package com.example.velox;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class admin_Configuracion extends AppCompatActivity {
+    private ImageView usuarios;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_configuracion);
+        bindUI();
+        usuarios.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoMain();
+            }
+        });
+    }
+    private void bindUI(){
+        usuarios = (ImageView) findViewById(R.id.btnUsuarios);
+    }
+    private void gotoMain(){
+        Intent intent = new Intent (admin_Configuracion.this, usuarios_admin.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/velox/inicioAdmin.java
+++ app/src/main/java/com/example/velox/inicioAdmin.java
@@ -2,13 +2,32 @@ package com.example.velox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class inicioAdmin extends AppCompatActivity {
-
+        private ImageView btnConf;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_admin);
+        bindUI();
+
+        btnConf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoMain();
+            }
+        });
+    }
+    private void bindUI(){
+        btnConf = (ImageView) findViewById(R.id.btnConfiguracion);
+    }
+    private void gotoMain(){
+        Intent intent = new Intent(inicioAdmin.this, admin_Configuracion.class);
+        startActivity(intent);
     }
 }
